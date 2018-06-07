5aa509411a0ba148d205ffedfa154f2174e82153
chun wang <q60563@gmail.com>
2018-06-07 16:34:58 +0800

    move to koa1

---
+++ koa1/koaExample/guestbook/app.js
@@ -0,0 +1,50 @@
+var koa = require('koa');
+var bodyParser = require('koa-bodyparser');
+var Router = require('koa-router');
+var views = require('koa-views');
+var serve = require('koa-static');
+
+var app = koa();
+var router = new Router();
+
+// app.use(views(__dirname + '/views', {
+//   extension: 'pug',
+//   map: {
+//     html: 'pug'
+//   }
+// }));
+
+app.use(views(__dirname + '/views', {
+  map: {
+    html: 'ejs'
+  }
+}));
+
+var msgs = [
+  { name: 'Fred', msg: 'Hi' },
+  { name: 'Boy', msg: 'Hello' }
+];
+
+router.get('/', function *() {
+  yield this.render('index', { msgs: msgs });
+});
+
+router.get('/leavemsg', function *() {
+  yield this.render('leavemsg');
+});
+
+router.post('/post', function *() {
+  var msg = {
+    name: this.request.body.name,
+    msg: this.request.body.msg
+  };
+
+  msgs.push(msg);
+
+  this.redirect('/');
+});
+
+app.use(bodyParser());
+app.use(serve('./public'));
+app.use(router.middleware());
+app.listen(3000);
